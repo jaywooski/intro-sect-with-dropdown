6df76c9e97f635b3c6cbfa6c088864cec072ce40
Jaywooski <john.m.wooley@gmail.com>
2022-09-24 22:59:28 -0500

    Added fucntionality for dropdown menu

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,14 @@
 const menuBtn = document.querySelector('.menu-btn');
 const closeBtn = document.querySelector('#close-btn')
 const navModal = document.querySelector('.nav-background');
+const featDown = document.querySelector('#features-drop');
+const featUp = document.querySelector('#featUp');
+const companyDrop = document.querySelector('#company-drop');
+const features = document.querySelector('#features-list');
 
+
+
+// Modal functionality
 const openModal = (event) => {
     navModal.style.display = 'block';
 }
@@ -16,5 +23,21 @@ window.onclick = function(event) {
     }
 }
 
+// Dropdown functionality
+const openFeatures = (event) => {
+    features.style.display = 'inline-block';
+    featDown.innerHTML = '<img src="./images/icon-arrow-up.svg" alt="Company Menu">'
+    featDown.id = 'featUp';
+}
+
+const closeFeatures = (event) => {
+    console.log('click');
+    features.style.display = 'none';
+    featUp.id = 'featDown';
+    featDown.innerHTML = '<img src="./images/icon-arrow-down.svg" alt="Company Menu">';
+}
+
 menuBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal)
+featDown.addEventListener('click', openFeatures);
+featUp.addEventListener('click', closeFeatures);
